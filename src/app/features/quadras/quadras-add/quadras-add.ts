ea4c677cec709918fcfb689abc34e7ddb45791d3
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2025-11-17 21:32:30 -0300

    feat:front adiciona funcionalidade de cadastro de quadras com novos campos e estilos

--- src/app/features/quadras/quadras-add/quadras-add.ts
+++ src/app/features/quadras/quadras-add/quadras-add.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/service/authService/auth-service';
+import { QuadraService } from '../../../core/service/quadraService/quadra-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-quadras-add',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './quadras-add.html',
   styleUrl: './quadras-add.scss'
 })
 export class QuadrasAdd {
+  quadraService = inject(QuadraService);
+  authService = inject(AuthService);
+
+  newQuadra = {
+    nome: '',
+    tipoQuadra: '',
+    proprietario: localStorage.getItem("currentUser"),
+    descricao: '',
+    valorHora: 0,
+    partidaGravavel: false,
+    estado: '',
+    cidade: '',
+    bairro: ''
+  };
 
 }
