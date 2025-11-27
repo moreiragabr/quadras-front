066f72f5dbd9083de4a69d0ae8004ace3950594f
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2025-11-27 16:45:33 -0300

    feat:front removendo testes desnecessários e renovando set-env para configuração de build

--- src/environments/set-env.ts
+++ src/environments/set-env.ts
@@ -3,22 +3,30 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { config } from 'dotenv';
 
-// Configura dotenv
+// Configura dotenv para ler o .env
 config();
 
 // Obter __dirname em módulos ES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const environmentContent = `export const environment = {
-  production: ${process.env['PRODUCTION'] || false},
+const devContent = `export const environment = {
+  production: false,
   locationIqApiKey: "${process.env['LOCATION_IQ_API_KEY'] || ''}",
+  SERVIDOR: "${process.env['API_URL_DEV'] || 'http://localhost:8080'}"
 };
 `;
 
-// Caminho para o environment.ts
-const targetPath = join(__dirname, 'environment.ts');
+const prodContent = `export const environment = {
+  production: true,
+  locationIqApiKey: "${process.env['LOCATION_IQ_API_KEY'] || ''}",
+  SERVIDOR: "${process.env['API_URL_PROD'] || 'http://fallback-ip:8080'}" // Variável PROD (IP Público)
+};
+`;
 
-// Escreve o arquivo
-writeFileSync(targetPath, environmentContent, 'utf8');
-console.log('✅ Environment file generated successfully!');
+const devPath = join(__dirname, 'environment.ts');
+const prodPath = join(__dirname, 'environment.prod.ts');
+
+writeFileSync(devPath, devContent, 'utf8');
+writeFileSync(prodPath, prodContent, 'utf8'); // <-- GERA O ARQUIVO DE PRODUÇÃO
+console.log('✅ Environment files generated successfully!');
